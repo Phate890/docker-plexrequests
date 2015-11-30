3ec17c7e6d15770bdd58fbe86fc33f500fce5ed2
aptalca <thedjinn@yahoo.com>
2015-11-30 16:11:43 -0500

    Update firstrun.sh

--- firstrun.sh
+++ firstrun.sh
@@ -5,6 +5,9 @@ cd /config
 if [ ! -d "/home/.meteor" ]; then
   echo "Fresh container detected; installing Meteor. This may take 10+ minutes!!"
   /sbin/setuser nobody curl https://install.meteor.com/ | /sbin/setuser nobody sh
+else
+  echo "Restarted container, Meteor already installed"
+fi
 
 if [ ! -d "/config/plexrequests-meteor" ]; then
   echo "First install detected, cloning PlexRequests repository"
